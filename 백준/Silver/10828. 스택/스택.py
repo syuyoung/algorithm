8c52bdbbd7f6d2f7df08b7bb90d7a59618b531ca
yuyoung <121468178+syuyoung@users.noreply.github.com>
2023-02-10 08:53:13 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+stack = []
+
+for _ in range(int(input())):
+    word = input().split()
+    if word[0] == 'push':
+        stack.append(word[1])
+
+    elif word[0] == 'pop':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop())
+    
+    elif word[0] == 'size':
+        print(len(stack))
+    
+    elif word[0] == 'empty':
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+
+    elif word[0] == 'top':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
