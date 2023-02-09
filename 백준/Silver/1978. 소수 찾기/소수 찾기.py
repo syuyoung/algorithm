44a05a6ab7024bd1c2fd82040f392b31e5a7390e
yuyoung <121468178+syuyoung@users.noreply.github.com>
2023-02-09 16:15:30 +0900

    [Silver V] Title: 소수 찾기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+numbers = list(map(int,input().split()))
+
+cnt = 0
+
+for num in numbers:
+    error = 0
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                error += 1
+        if error == 0:
+            cnt += 1
+print(cnt)
