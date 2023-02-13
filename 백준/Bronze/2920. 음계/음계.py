f07c1dd7aa36a82ae134a82dbf16c95e8b511f6e
yuyoung <121468178+syuyoung@users.noreply.github.com>
2023-02-13 15:07:14 +0900

    [Bronze II] Title: 음계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -1,13 +1,10 @@
 import sys
 input = sys.stdin.readline
 
-asc = [1, 2, 3, 4, 5, 6, 7, 8]
-desc = [8, 7, 6, 5, 4, 3, 2, 1]
-
 N = list(map(int,input().split()))
-if N == asc:
+if N == sorted(N):
     print('ascending')
-elif N == desc:
+elif N == sorted(N, reverse=True):
     print('descending')
 else:
     print('mixed')
